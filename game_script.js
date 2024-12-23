d3640b6846ccf2364755449289f6fbb72e8373ac
Martin Balciar <martin.balciar96@gmail.com>
2024-12-23 10:21:02 +0100

    Second commit
    - Added new sprites for plane animations
    - Added array with all the possible coordinates of the sprites
    - Changed the way animation calculates next frame to automatic instead of hard coded

--- game_script.js
+++ game_script.js
@@ -1,41 +1,64 @@
 //Load in the canvas for the game
 const gameboard = document.getElementById('gameboard');
 const ctx = gameboard.getContext('2d');
-//console.log(ctx);
 
 //Set the size of the board = size of the canvas
 const board_width = gameboard.width = 600;
 const board_height = gameboard.height = 600;
 
 //Constructor for Image object
-const idlePlaneImage = new Image();
-idlePlaneImage.src = 'sprites/sprite_plane_idle.png';
+const PlaneImage = new Image();
+PlaneImage.src = 'sprites/plane_sprites.png';
 
 const spriteWidth = 600;
 const spriteHeight = 600;
 
-let idleAnimPos = 0;
-let idleAnimNum = 6;
+const planeAnims = [];
+
+const animsStates = [{
+    name: 'idle', frames: 6,
+},
+{
+    name: 'moveUp', frames: 5,
+}];
+
+//Fil the array with all the posible coordinates for the sprites
+animsStates.forEach((state,index) => {
+    let frames = {
+        location: [],
+    }
+    for(let i = 0; i < state.frames; i++){
+        let positionX = spriteWidth * i;
+        let positionY = spriteHeight * index;
+        frames.location.push({x: positionX, y: positionY});
+    }
+    planeAnims[state.name] = frames;
+});
+console.log(animsStates);
+console.log(planeAnims);
+
+
 
 let gameFrame = 0;
-const slowDown = 5;
+const slowDownIdle = 5;
+const slowDownMotion =8;
 
-function animatePlaneIdle(){
-    
-    //clear out the canvas at start
+let AnimPosX = 0;
+function animatePlane(){
+
+    let AnimPosY = planeAnims['idle'].location[AnimPosX].y;
     ctx.clearRect(0,0,board_width,board_height);
     //Src image, coordinates for cutting, size of the cut, placement of the image, size of the image
-    ctx.drawImage(idlePlaneImage,spriteWidth * idleAnimPos,0,spriteWidth,spriteHeight,0,200,200,200);
-    requestAnimationFrame(animatePlaneIdle);
-    //Slow down the animation a bit
-    if(gameFrame % slowDown==0 ){
-        if(idleAnimPos < idleAnimNum-1){
-            idleAnimPos++;
+    ctx.drawImage(PlaneImage,spriteWidth * AnimPosX,AnimPosY,spriteWidth,spriteHeight,0,200,200,200);
+    requestAnimationFrame(animatePlane);
+    if(gameFrame % slowDownMotion ==0 ){
+        if(AnimPosX < planeAnims['idle'].location.length -1){
+            AnimPosX++;
         }else{
-            idleAnimPos=0;
+            AnimPosX=0;
         }
     }
     gameFrame++;
+    //console.log(AnimPosX,AnimPosY);
 }
-animatePlaneIdle();
-
+animatePlane();
