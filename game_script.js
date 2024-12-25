d727de64bc7fce108c99609ba92530a9f6647595
Martin Balciar <martin.balciar96@gmail.com>
2024-12-25 10:13:57 +0100

    Third commit
    - Added sprites for the background
    - Added background animation
    - Cleaned up the code a little

--- game_script.js
+++ game_script.js
@@ -7,11 +7,21 @@ const board_width = gameboard.width = 600;
 const board_height = gameboard.height = 600;
 
 //Constructor for Image object
-const PlaneImage = new Image();
-PlaneImage.src = 'sprites/plane_sprites.png';
+const planeImage = new Image();
+planeImage.src = 'sprites/plane_sprites.png';
+const background1 = new Image();
+background1.src = 'sprites/sprite_commie_blocks_2.png';
+const background2 = new Image();
+background2.src = 'sprites/sprite_sky_2.png';
 
+//Default size of the sprites
 const spriteWidth = 600;
 const spriteHeight = 600;
+
+let gameFrame = 0;
+const slowDown = 8;
+let AnimPosX = 0;
+let planeState = 'moveUp';
 
 const planeAnims = [];
 
@@ -34,25 +44,18 @@ animsStates.forEach((state,index) => {
     }
     planeAnims[state.name] = frames;
 });
-console.log(animsStates);
-console.log(planeAnims);
-
-
-
-let gameFrame = 0;
-const slowDownIdle = 5;
-const slowDownMotion =8;
-
-let AnimPosX = 0;
+//console.log(animsStates);
+//console.log(planeAnims);
+/*
+//Animation of the plane
 function animatePlane(){
-
-    let AnimPosY = planeAnims['idle'].location[AnimPosX].y;
     ctx.clearRect(0,0,board_width,board_height);
+    let AnimPosY = planeAnims[planeState].location[AnimPosX].y;
     //Src image, coordinates for cutting, size of the cut, placement of the image, size of the image
-    ctx.drawImage(PlaneImage,spriteWidth * AnimPosX,AnimPosY,spriteWidth,spriteHeight,0,200,200,200);
+    ctx.drawImage(planeImage,spriteWidth * AnimPosX,AnimPosY,spriteWidth,spriteHeight,0,200,200,200);
     requestAnimationFrame(animatePlane);
-    if(gameFrame % slowDownMotion ==0 ){
-        if(AnimPosX < planeAnims['idle'].location.length -1){
+    if(gameFrame % slowDown ==0 ){
+        if(AnimPosX < planeAnims[planeState].location.length -1){
             AnimPosX++;
         }else{
             AnimPosX=0;
@@ -62,3 +65,31 @@ function animatePlane(){
     //console.log(AnimPosX,AnimPosY);
 }
 animatePlane();
+*/
+
+
+//Background layers animated
+let backgroundPos1 = 0;
+let backgroundPos2 = 0;
+
+function animateBackground(){
+    ctx.clearRect(0,0,board_width,board_height);
+    ctx.drawImage(background2,backgroundPos2,0);
+    ctx.drawImage(background2,backgroundPos2-spriteWidth,0);
+    if(backgroundPos2 >= 600){
+        backgroundPos2 =0;
+    }else{
+        backgroundPos2 +=0.1;
+        //console.log(backgroundPos2);
+    }
+    ctx.drawImage(background1,backgroundPos1,300);
+    ctx.drawImage(background1,backgroundPos1+spriteWidth,300);
+    if(backgroundPos1 == -600){
+        backgroundPos1 = 0;
+    }else{
+        backgroundPos1 -= 1.5;
+    }
+    requestAnimationFrame(animateBackground);
+
+}
+animateBackground();
